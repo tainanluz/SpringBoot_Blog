098a26345f6bf6af622c2f9388546f3b3a708f7a
unknown <contato.tainanluz@gmail.com>
2021-09-13 13:43:11 -0300

    [Feature Tai - Task: Create security package and Create classes BasicSecurityConfig, UserDetailsImplements, UserDetailsServiceImplements]

--- src/main/java/com/blogdev/blogdev/Repository/UsuarioRepository.java
+++ src/main/java/com/blogdev/blogdev/Repository/UsuarioRepository.java
@@ -1,5 +1,8 @@
 package com.blogdev.blogdev.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,12 @@ import com.blogdev.blogdev.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	// Método utilizado para pesquisar coluna nome ContainigIgnoreCase
+	List<Usuario> findAllByNome(String nome);
+	
+	//Método utilizado para pesquisar coluna email
+	Optional<Usuario> findByEmail(String email);
+	
 
 }
