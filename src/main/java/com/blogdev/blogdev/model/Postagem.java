243bc987565afd9a07087eee78535498f3688207
unknown <contato.tainanluz@gmail.com>
2021-09-13 14:45:46 -0300

    [Feature Tai - Task: Create relationship between entities]

--- src/main/java/com/blogdev/blogdev/model/Postagem.java
+++ src/main/java/com/blogdev/blogdev/model/Postagem.java
@@ -4,13 +4,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class Postagem {
 	private @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long idPostagem;
 	private @NotBlank String titulo;
 	private @NotBlank String descricao;
+	
+	@ManyToOne
+	@JoinColumn(name = "criador_id")
+	@JsonIgnoreProperties({"minhasPostagens"})
+	private Usuario criador;
+
+	@ManyToOne
+	@JoinColumn(name = "tema_id")
+	@JsonIgnoreProperties({"postagens"})
+	private Tema temaRelacionado;
 	
 	public Long getIdPostagem() {
 		return idPostagem;
@@ -30,4 +44,17 @@ public class Postagem {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	public Usuario getCriador() {
+		return criador;
+	}
+	public Tema getTemaRelacionado() {
+		return temaRelacionado;
+	}
+	public void setCriador(Usuario criador) {
+		this.criador = criador;
+	}
+	public void setTemaRelacionado(Tema temaRelacionado) {
+		this.temaRelacionado = temaRelacionado;
+	}
+	
 }
--- src/main/java/com/blogdev/blogdev/model/Usuario.java
+++ src/main/java/com/blogdev/blogdev/model/Usuario.java
@@ -1,19 +1,30 @@
 package com.blogdev.blogdev.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class Usuario {
 	private  @Id @GeneratedValue (strategy = GenerationType.IDENTITY)Long idUsuario;
 	private @NotBlank String nome;
 	private @NotBlank @Email String email;
-	private @NotBlank @Size (min=5) String senha;
+	private @NotBlank String senha;
+	
+	@OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"criador"})
+	private List<Postagem> minhasPostagens = new ArrayList<>();
 
 public Long getIdUsuario() {
 	return idUsuario;
@@ -39,5 +50,12 @@ public String getSenha() {
 public void setSenha(String senha) {
 	this.senha = senha;
 }
+public List<Postagem> getMinhasPostagens() {
+	return minhasPostagens;
+}
+public void setMinhasPostagens(List<Postagem> minhasPostagens) {
+	this.minhasPostagens = minhasPostagens;
+}
+
 
 }
